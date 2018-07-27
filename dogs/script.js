1f94014ed23b8be00f2321af061088bd1c3b4eed
Volodymyr Lukovyna <volodymyrlukovyna@MacBook-Pro-Volodymyr.local>
2018-07-27 17:55:51 +0300

    ES5, ES6 classes

---
+++ dogs/script.js
@@ -0,0 +1,42 @@
+// var Animal = function(){};
+
+// var Dog = function(name){
+//     this.name = name 
+// };
+
+
+//     Animal.prototype.getName = function() {
+//     return this.name 
+// }
+
+// Dog.prototype = Object.create(Animal.prototype);
+
+// Dog.prototype.bark = function(){
+//     console.log('Dog ' + this.name + ' is barking')
+// };
+
+// var dog = new Dog("Sharik")
+
+//------------------ ES6 -------------------
+
+    
+// class Animal {
+//     constructor (name) {
+//         this.name = name
+//     }
+//     getName () {
+//         return this.name
+//     }
+// }
+
+// class Dog extends Animal{
+//     constructor (name) {
+//         super (name)
+//     }
+//     bark () {
+//         console.log (`Dog ${this.name} is barking`)
+//     }
+// }
+
+// const dog = new Dog("Tuzik")
+
