80b350af4d172a60d04b53d6c5489b80ed996c65
Volodymyr Lukovyna <vladimir.lukovina@gmail.com>
2018-07-31 16:52:04 +0300

    add findNeighbours function, need fix

--- minigame/main.js
+++ minigame/main.js
@@ -1,7 +1,6 @@
 window.onload = function(){  
-    let container = document.querySelector(".container")
-
-    let field = new Array(7)
+    let container = document.querySelector(".container"),
+        field = new Array(7)
 
     for(i = 0; i < field.length; i++){
         field[i] = new Array(6);
@@ -11,20 +10,50 @@ window.onload = function(){
     } 
 
     function arrayRender(arr){
+        let count = 0;
         for(var i = 0; i < arr.length; i++){
             for(var j = 0; j < arr[i].length; j++){
-                container.innerHTML+= `<div class='cell cell-${arr[i][j]}'><img src='./img/${arr[i][j]}.png'></div>`  ;
+                container.innerHTML+= `<div class='cell cell-${arr[i][j]}' id="id-${count}"><img src='./img/${arr[i][j]}.png'></div>`  ;
+                count++
             }
             container.innerHTML+= "</br>"
         }     
           let cells = document.querySelectorAll(".cell")
-          cells.forEach(item=>item.addEventListener("click", clearCell))
+          cells.forEach(item=>item.addEventListener("click", ()=>findNeighbours(item)))
     }
 
-    function clearCell() {
-        this.classList = "cell"
-        this.innerHTML = ""
-        console.dir(this)
+    function findNeighbours(target){
+        console.log(target)
+        let num = target.id.slice(3);
+        let bottomNeighbour = document.querySelector(`#id-${+num + 6}`)
+        let upperNeighbour = document.querySelector(`#id-${+num - 6}`)
+        let rightNeighbour = document.querySelector(`#id-${+num + 1}`)
+        let leftNeighbour = document.querySelector(`#id-${+num - 1}`)
+
+        if(bottomNeighbour.classList.value == target.classList.value) {
+            if(bottomNeighbour){findNeighbours(bottomNeighbour)}
+            clearCell(bottomNeighbour);
+        }
+        if(upperNeighbour.classList.value == target.classList.value) {
+            if(upperNeighbour){findNeighbours(upperNeighbour)}
+            clearCell(upperNeighbour);
+        }
+        if(rightNeighbour.classList.value == target.classList.value) {
+            if(rightNeighbour){findNeighbours(rightNeighbour)}
+            clearCell(rightNeighbour);
+        }
+        if(leftNeighbour.classList.value == target.classList.value) {
+            if(leftNeighbour){findNeighbours(leftNeighbour)}
+            clearCell(leftNeighbour);
+        }
+        
+        clearCell(target)
+    }
+
+    function clearCell(cell) {
+        // target.classList = "cell"
+        cell.innerHTML = ""
+        console.dir(cell)
     }
 
 
