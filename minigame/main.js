4858bee2c1af5feb4ec58d3d5b7ea648d57184bd
Volodymyr Lukovyna <volodymyrlukovyna@MacBook-Pro-Volodymyr.local>
2018-07-31 23:07:27 +0300

    ff

--- minigame/main.js
+++ minigame/main.js
@@ -1,55 +1,68 @@
-window.onload = function(){  
-    let container = document.querySelector(".container"),
-        field = new Array(7)
+window.onload = function() {
 
-    for(i = 0; i < field.length; i++){
+    let container = document.querySelector('.container'),
+        field     = new Array(7);
+
+    for (let i = 0; i < field.length; i++) {
         field[i] = new Array(6);
-        for(var j = 0; j < field[i].length; j++){
-            field[i][j] = Math.floor(Math.random()*4)
+        for (let j = 0; j < field[i].length; j++) {
+        field[i][j] = Math.floor(Math.random() * 4);
         }
-    } 
-
-    function arrayRender(arr){
+    }
+   
+    function arrayRender(arr) {
         let count = 0;
-        for(var i = 0; i < arr.length; i++){
-            for(var j = 0; j < arr[i].length; j++){
-                container.innerHTML+= `<div class='cell cell-${arr[i][j]}' id="id-${count}"><img src='./img/${arr[i][j]}.png'></div>`  ;
-                count++
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = 0; j < arr[i].length; j++) {
+                container.innerHTML += `
+                    <div class='cell cell-${arr[i][j]}' id="id-${count}">
+                        <img src='./img/${arr[i][j]}.png'>
+                    </div>`;
+                count++;
             }
-            container.innerHTML+= "</br>"
-        }     
-          let cells = document.querySelectorAll(".cell")
-          cells.forEach(item=>item.addEventListener("click", ()=>findNeighbours(item)))
+        container.innerHTML += '</br>';
     }
 
-    arrayRender(field)
-
-    async function findNeighbours(target){
-        let num = target.id.slice(3),
-            bottomNeighbour = document.querySelector(`#id-${+num + 6}`),
-            upperNeighbour = document.querySelector(`#id-${+num - 6}`),
-            rightNeighbour = document.querySelector(`#id-${+num + 1}`),
-            leftNeighbour = document.querySelector(`#id-${+num - 1}`);
-
-        // if(bottomNeighbour && bottomNeighbour.classList.value == target.classList.value){
-        //     findNeighbours(bottomNeighbour)
-        // }    
-        // if(upperNeighbour && upperNeighbour.classList.value == target.classList.value) {
-        //     findNeighbours(upperNeighbour)
-        // }
-        // if(rightNeighbour && rightNeighbour.classList.value == target.classList.value) {
-        //     findNeighbours(rightNeighbour)
-        // }
-        if(leftNeighbour && leftNeighbour.classList.value == target.classList.value) {
-            findNeighbours(leftNeighbour)
-        }
-        
-        clearCell(target)
-    }
-
-    function clearCell(cell) {
-        cell.classList = "cell"
-        cell.innerHTML = ""
-    }
-
+let cells = document.querySelectorAll('.cell');
+cells.forEach(item => item.addEventListener('click', () => findNeighbours(item)));
 }
+   
+    arrayRender(field);
+   
+    function findNeighbours(target) {
+        let num          = target.id.slice(3);
+        let nextElements = [];
+
+        let bottomNeighbour = document.querySelector(`#id-${+num + 6}`);
+            if (bottomNeighbour && bottomNeighbour.classList.value === target.classList.value) {
+            nextElements.push(bottomNeighbour);
+        }
+        let upperNeighbour = document.querySelector(`#id-${+num - 6}`);
+            if (upperNeighbour && upperNeighbour.classList.value === target.classList.value) {
+            nextElements.push(upperNeighbour);
+        }
+        let rightNeighbour = document.querySelector(`#id-${+num + 1}`);
+            if (rightNeighbour && rightNeighbour.classList.value === target.classList.value) {
+            nextElements.push(rightNeighbour);
+        }
+        let leftNeighbour = document.querySelector(`#id-${+num - 1}`);
+            if (leftNeighbour && leftNeighbour.classList.value === target.classList.value) {
+            nextElements.push(leftNeighbour);
+        }
+     // clear current cell
+     clearCell(target);
+   
+     // find and clear neighbours for all others
+    nextElements.forEach(el => {
+        findNeighbours(el);
+        clearCell(el)
+        });
+    }
+   
+    function clearCell(cell) {
+        cell.classList = 'cell';
+        cell.innerHTML = '';
+    }
+   
+};
+
