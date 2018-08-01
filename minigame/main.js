a240c9f55642dfb70267033eb2332cca9687b7e5
Volodymyr Lukovyna <vladimir.lukovina@gmail.com>
2018-08-01 10:53:34 +0300

    add nextElements array

--- minigame/main.js
+++ minigame/main.js
@@ -1,44 +1,40 @@
 window.onload = function() {
-
     let container = document.querySelector('.container'),
         field     = new Array(7);
-
-    for (let i = 0; i < field.length; i++) {
+   
+    for (i = 0; i < field.length; i++) {
         field[i] = new Array(6);
-        for (let j = 0; j < field[i].length; j++) {
-        field[i][j] = Math.floor(Math.random() * 4);
-        }
+        for (var j = 0; j < field[i].length; j++) {
+            field[i][j] = Math.floor(Math.random() * 4);
+         }
     }
    
     function arrayRender(arr) {
         let count = 0;
-        for (let i = 0; i < arr.length; i++) {
-            for (let j = 0; j < arr[i].length; j++) {
-                container.innerHTML += `
-                    <div class='cell cell-${arr[i][j]}' id="id-${count}">
-                        <img src='./img/${arr[i][j]}.png'>
-                    </div>`;
+        for (var i = 0; i < arr.length; i++) {
+            for (var j = 0; j < arr[i].length; j++) {
+                container.innerHTML += `<div class='cell cell-${arr[i][j]}' id="id-${count}"><img src='./img/${arr[i][j]}.png'></div>`;
                 count++;
             }
-        container.innerHTML += '</br>';
+            container.innerHTML += '</br>';
+        }
+     let cells = document.querySelectorAll('.cell');
+     cells.forEach(item => item.addEventListener('click', () => findNeighbours(item)));
     }
-
-let cells = document.querySelectorAll('.cell');
-cells.forEach(item => item.addEventListener('click', () => findNeighbours(item)));
-}
    
     arrayRender(field);
    
     function findNeighbours(target) {
-        let num = target.id.slice(3),
+        let num          = target.id.slice(3),
             nextElements = [],
             bottomNeighbour = document.querySelector(`#id-${+num + 6}`),
             upperNeighbour = document.querySelector(`#id-${+num - 6}`),
             rightNeighbour = document.querySelector(`#id-${+num + 1}`),
-            leftNeighbour = document.querySelector(`#id-${+num - 1}`)
-
+            leftNeighbour = document.querySelector(`#id-${+num - 1}`);
+   
+        
         if (bottomNeighbour && bottomNeighbour.classList.value === target.classList.value) {
-            nextElements.push(bottomNeighbour);
+             nextElements.push(bottomNeighbour);
         }
         if (upperNeighbour && upperNeighbour.classList.value === target.classList.value) {
             nextElements.push(upperNeighbour);
@@ -49,19 +45,19 @@ cells.forEach(item => item.addEventListener('click', () => findNeighbours(item))
         if (leftNeighbour && leftNeighbour.classList.value === target.classList.value) {
             nextElements.push(leftNeighbour);
         }
-
+     // clear current cell
      clearCell(target);
-   
-    nextElements.forEach(el => {
-        findNeighbours(el);
-        clearCell(el)
-        });
+
+     // find and clear neighbours for all others
+     nextElements.forEach(el => {
+      findNeighbours(el);
+      clearCell(el);
+     });
     }
    
     function clearCell(cell) {
-        cell.classList = 'cell';
-        cell.innerHTML = '';
+     cell.classList = 'cell';
+     cell.innerHTML = '';
     }
    
-};
-
+   };
